da44394ed60e8a3c4091a836f2fa47fa9a78aec2
Ridju <gitmail2@protonmail.com>
2023-11-11 19:09:18 +0100

    added auth middleware

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/Ridju/ticktr/internal/token"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	AuthorizationHeaderKey  = "authorization"
+	AuthorizationTypeBearer = "bearer"
+	AuthorizationPayloadKey = "authorization_payload"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader(AuthorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != AuthorizationTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", authorizationType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		ctx.Set(AuthorizationPayloadKey, payload)
+		ctx.Next()
+	}
+}
